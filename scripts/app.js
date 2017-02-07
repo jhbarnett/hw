14701daccbe2cc750eab02aa4f044de82de9bc9e
Jason Barnett <jason@thebarnetts.ws>
2017-02-07 11:39:57 -0600

    completes MVP requirements

--- scripts/app.js
+++ scripts/app.js
@@ -1,10 +1,42 @@
 angular
-    .module('webapp', ['ngMaterial', 'ngMessages', 'ngResource'])
-    .controller('DemoCtrl', function($scope, $http, $resource) {
+    .module('Beer', ['ngMaterial', 'ngMessages', 'ngResource'])
+    .controller('BeerCtrl', function($scope, $http, $resource) {
+        fetchBeerList()
+        
+        $scope.beerList = []
 
+        function fetchBeerList(){
+          return $http.get('https://api.punkapi.com/v2/beers')
+          .then(beers => 
+            $scope.beerList =
+                beers.data.map(beer => {
+                  return {
+                    abv: beer.abv,
+                    tagline: beer.tagline,
+                    image: beer.image_url,
+                    name: beer.name,
+                  }    
+                })
+          )
+        } 
 
-        // use $http or $resource
-        // https://api.punkapi.com/v2/beers
+        $scope.$on('NameSort', function(){
+            $scope.beerList.sort((a, b) => {
+                const nameA = a.name.toUpperCase() 
+                const nameB = b.name.toUpperCase()
+                if (nameA < nameB) { return -1 }
+                if (nameA > nameB) { return 1 }
+                // names must be equal
+                return 0
+            })
+        })
+
+        $scope.$on('ABVSort', function(){
+            $scope.beerList.sort((a, b) => {
+                return a.abv - b.abv
+            })
+        })
+
 
         
     })
